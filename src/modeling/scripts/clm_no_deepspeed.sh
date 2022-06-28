aaeecbb75b16d5f3c13c6c2be95fe18bbba96a3c
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2022-06-28 09:51:32 +0530

    adding code

---
+++ src/modeling/scripts/clm_no_deepspeed.sh
@@ -0,0 +1,13 @@
+accelerate launch ./../run_clm_no_trainer.py \
+--config_name "gpt2-xl" \
+--tokenizer_name "gpt2-xl" \
+--dataset_name "wikitext" \
+--dataset_config_name "wikitext-2-raw-v1" \
+--block_size 128 \
+--output_dir "./clm/clm_deepspeed_stage2_accelerate" \
+--learning_rate 5e-4 \
+--per_device_train_batch_size 1 \
+--per_device_eval_batch_size 1 \
+--num_train_epochs 1 \
+--with_tracking \
+--report_to "wandb"\
---
+++ src/modeling/scripts/cls_no_deepspeed.sh
@@ -0,0 +1,11 @@
+time accelerate launch ./../run_cls_no_trainer.py \
+  --model_name_or_path "microsoft/deberta-v2-xlarge-mnli" \
+  --task_name "mrpc" \
+  --ignore_mismatched_sizes \
+  --max_length 128 \
+  --per_device_train_batch_size 8 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --output_dir "/tmp/mrpc/" \
+  --with_tracking \
+  --report_to "wandb" \
